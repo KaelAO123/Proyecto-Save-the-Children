7bb6a648351c5abfcca5470c4a46b673d0f15a57
Erickxdps <juliopicavia0000@gmail.com>
2024-11-03 17:46:07 -0400

    demas

--- templates/frontendmejorado/Assets/JavaScript/Main.js
+++ templates/frontendmejorado/Assets/JavaScript/Main.js
@@ -1,19 +1,8 @@
-/*==================== Menú pegajoso ====================*/
-
-// Agrega un listener al evento "scroll" de la ventana
 window.addEventListener('scroll', function () {
-
-    // Selecciona el elemento con el ID "Sticky"
-    var navbar = document.querySelector('#Sticky');
-
-    // Verifica si el desplazamiento vertical de la ventana es mayor a 0
-    if (window.pageYOffset > 0) {
-
-      // Si es así, agrega la clase "scrolled" al elemento "navbar"
-      navbar.classList.add('scrolled');
-    } else {
-
-      // Si no, remove la clase "scrolled" del elemento "navbar"
-      navbar.classList.remove('scrolled');
-    }
-  });
+  var navbar = document.querySelector('#Sticky');
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
